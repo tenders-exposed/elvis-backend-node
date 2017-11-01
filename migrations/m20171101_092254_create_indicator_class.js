9acb332361f468df1341deff9cb5ecdf08803723
Georgiana Bere <bere.georgi@gmail.com>
2017-11-01 13:36:14 +0200

    Add indicators to db schema

---
+++ migrations/m20171101_092254_create_indicator_class.js
@@ -0,0 +1,42 @@
+'use strict';
+
+exports.name = 'create indicator class';
+
+exports.up = (db) => (
+  db.class.create('Indicator')
+    .then((Indicator) => {
+      Indicator.property.create([
+        {
+          name: 'id',
+          type: 'String',
+          mandatory: true,
+        },
+        {
+          name: 'xDigiwhistLastModified',
+          type: 'DateTime',
+        },
+        {
+          name: 'type',
+          type: 'String',
+        },
+        {
+          name: 'value',
+          type: 'double',
+        },
+        {
+          name: 'relatedEntityId',
+          type: 'String',
+        },
+      ]);
+    })
+    .then(() => {
+      db.index.create({
+        name: 'Indicator.id',
+        type: 'unique',
+      });
+    })
+);
+
+exports.down = (db) => (
+  db.class.drop('Indicator')
+);
---
+++ migrations/m20171101_105526_add_indicators_to_tender_bid_lot_actor.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+exports.name = 'add indicators to tender bid lot actor';
+
+exports.up = (db) => (
+  Promise.map(['Tender', 'Lot', 'Bid', 'Actor'], (className) => {
+    db.class.get(className)
+      .then((Class) => {
+        Class.property.create([
+          {
+            name: 'indicators',
+            type: 'EmbeddedSet',
+            linkedClass: 'Indicator',
+          },
+        ]);
+      });
+  })
+);
+
+exports.down = (db) => (
+  Promise.map(['Tender', 'Lot', 'Bid', 'Actor'], (className) => {
+    db.class.get(className)
+      .then((Class) => Class.property.drop('indicators'));
+  })
+);
