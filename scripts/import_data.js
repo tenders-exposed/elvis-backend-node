1d773e71511b4ea2a0dad9ec2e54f23d574c7e46
Georgiana Bere <bere.georgi@gmail.com>
2017-11-07 19:54:55 +0200

    Unfinished import data that contains the steps I should take

--- scripts/import_data.js
+++ scripts/import_data.js
@@ -7,98 +7,98 @@ const config = require('./../config');
 const tenderExtractor = require('./../extractors/tender');
 const buyerExtractor = require('./../extractors/buyer');
 
-// Alternative implementation where the update/create logic happens in the `then` of `one()`
-// Same problem as below, `existingRecord` is always undefined thus `update` never happens and the `created` done instead fails
-function upsertVertex(transaction, className, attrs) {
-  const recordName = `${className.toLowerCase()}${attrs.id.replace(/-/g, '')}`;
-  config.db.select().from(className).where({ id: attrs.id }).one()
-    .then((existingRecord) => {
-      transaction.let(recordName, (t) => {
-        if (_.isUndefined(existingRecord)) {
-          transaction.create('vertex', className)
-            .set(attrs);
+function recordName(uuid, className) {
+  return `${className.toLowerCase()}${uuid.replace(/-/g, '')}`;
+}
+
+function upsertBuyer(transaction, rawBuyer, rawTender) {
+  const buyer = buyerExtractor.extractBuyer(rawBuyer);
+  const buyerName = recordName(buyer.id, 'Buyer');
+  const tenderName = recordName(rawTender.id, 'Tender');
+  const createsName = `${buyerName}creates${tenderName}`;
+  const fromName = `$${buyerName}`;
+  const toName = `$${tenderName}`;
+
+  return config.db.select().from('Buyer').where({ id: buyer.id }).one()
+    .then((existingBuyer) => {
+      transaction.let(buyerName, (t) => {
+        if (_.isUndefined(existingBuyer)) {
+          t.create('vertex', 'Buyer')
+            .set(buyer);
         } else {
-          t.update(className)
-            .set(attrs)
-            .where({ id: attrs.id })
+          t.update('Buyer')
+            .set({ buyerType: 'Alabalaportocala' })
+            .where({ id: buyer.id })
             .return('AFTER');
+          // trx.let(createsName, (t) => {
+          //   t.delete('edge', 'Creates')
+          //     .from(fromName)
+          //     .to(toName);
+          // });
         }
+      }).let(createsName, (t) => {
+        t.create('edge', 'Creates')
+          .from(fromName)
+          .to(toName)
+          .set(buyerExtractor.extractCreates(rawBuyer, rawTender));
       });
+      return transaction;
     });
 }
 
-function writeTender(tenderAttrs) {
-  const tender = tenderExtractor.extractTender(tenderAttrs);
-  function recordName(uuid, className) {
-    return `${className.toLowerCase()}${uuid.replace(/-/g, '')}`;
-  }
+function writeTender(rawTender) {
+  config.db.on("endQuery", function(obj) {
+    console.log("DEBUG QUERY:");
+    console.dir(obj.input);
+  });
+  const tender = tenderExtractor.extractTender(rawTender);
   const tenderName = recordName(tender.id, 'Tender');
 
-  // `existingTender` is always undefined thus `update` never happens and the `create` done instead fails
-  let existingTender;
   config.db.select().from('Tender').where({ id: tender.id }).one()
-    .then((t) => { existingTender = t; });
-
-  let trx = config.db.let(tenderName, (t) => {
-    if (_.isUndefined(existingTender)) {
-      t.create('vertex', 'Tender')
-        .set(tender);
-    } else {
-      t.update('Tender')
-        .set(tender)
-        .where({ id: tender.id })
-        .return('AFTER');
-    }
-  });
-
-  tenderAttrs.buyers.forEach((buyerAttrs) => {
-    const buyer = buyerExtractor.extractBuyer(buyerAttrs);
-    const buyerName = recordName(buyer.id, 'Buyer');
-    const createsName = `${buyerName}creates${tenderName}`;
-    const fromName = `$${buyerName}`;
-    const toName = `$${tenderName}`;
-    let existingBuyer;
-
-    config.db.select().from('Buyer').where({ id: buyer.id }).one()
-      .then((b) => { existingBuyer = b; });
-    trx.let(buyerName, (t) => {
-      if (_.isUndefined(existingBuyer)) {
-        t.create('vertex', 'Buyer')
-          .set(buyer);
-      } else {
-        t.update('Buyer')
-          .set(buyer)
-          .where({ id: buyer.id })
-          .return('AFTER');
-        // trx.let(createsName, (t) => {
-        //   t.delete('edge', 'Creates')
-        //     .from(fromName)
-        //     .to(toName);
-        // });
-      }
-    });
-    trx.let(createsName, (t) => {
-      t.create('edge', 'Creates')
-        .from(fromName)
-        .to(toName)
-        .set(buyerExtractor.extractCreates(buyerAttrs, tenderAttrs));
-    });
-  });
-
-  const returnName = `$${tenderName}`;
-  trx.commit().return(returnName).all()
-    .then((results) => { console.log(results); })
-    .catch((err) => {
-      console.log(err);
-      console.log(tenderAttrs);
+    .then((existingTender) => {
+      let transaction = config.db.let(tenderName, (t) => {
+        if (_.isUndefined(existingTender)) {
+          t.create('vertex', 'Tender')
+            .set(tender);
+        } else {
+          t.update('Tender')
+            .set(tender)
+            .where({ id: tender.id })
+            .return('AFTER');
+        }
+      });
+      return transaction;
+    })
+    .then((transaction) => {
+      Promise.all([
+        // createBuyers
+        Promise.each(rawTender.buyers, (rawBuyer) => {
+          upsertBuyer(transaction, rawBuyer, rawTender);
+        }),
+        // createLots,
+        // createCPVs,
+      ])
+        // .then(() => {
+        //   createAwarded edge between bids and buyers
+        // })
+        .then(() => {
+          const returnName = `$${tenderName}`;          
+          transaction.commit().return(returnName).all()
+            .then((results) => { console.log(results); })
+            .catch((err) => {
+              console.log(transaction._state.let);
+              console.log(err);
+              process.exit(-1);
+            });
+        });
     });
 }
 
 function importFileData(filePath) {
   const instream = fs.createReadStream(filePath);
-  const iterator = readline.createInterface({ input: instream });
+  const handler = readline.createInterface({ input: instream });
 
-  iterator.on('line', (line) => {
+  handler.on('line', (line) => {
     const tenderAttrs = JSON.parse(line);
     const awardedLots = _.compact((tenderAttrs.lots || []).map((lot) => {
       if ((lot.bids || []).filter((bid) => bid.isWinning === true).length >= 1) {
@@ -111,7 +111,7 @@ function importFileData(filePath) {
     writeTender(tenderAttrs);
   });
 
-  iterator.on('close', () => {
+  handler.on('close', () => {
   });
 }
 
@@ -121,4 +121,4 @@ function importDumpFiles() {
   dumpFilePaths.forEach((filePath) => importFileData(filePath));
 }
 
-importDumpFiles()
+importDumpFiles();
