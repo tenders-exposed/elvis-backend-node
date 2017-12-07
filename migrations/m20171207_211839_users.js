c734bf7c5b584a02ca0cd450fc20db4c33da3507
Edgar Markosov <edgar.markosov@gmail.com>
2017-12-08 03:42:22 +0400

    orient db migration, save in user class, jwt tokens (NOT TESTED)

---
+++ migrations/m20171207_211839_users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+exports.name = 'users';
+
+exports.up = (db) => {
+  db.class.create('Users')
+    .then((Users) => {
+      Users.property.create([
+        {
+          name: 'email',
+          type: 'String',
+          mandatory: true,
+        },
+        {
+          name: 'password',
+          type: 'String',
+        },
+        {
+          name: 'twitterId',
+          type: 'String',
+        },
+        {
+          name: 'githubId',
+          type: 'String',
+        },
+        {
+          name: 'accessTokens',
+          type: 'EmbeddedSet',
+        },
+        {
+          name: 'refreshTokens',
+          type: 'EmbeddedSet',
+        },
+      ]);
+    });
+};
+
+exports.down = (db) => {
+  db.class.drop('Users');
+};
