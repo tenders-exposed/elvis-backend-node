418033dd0cf5d8f13c6adeee671e91cee0bb0fbe
Edgar Markosov <edgar.markosov@gmail.com>
2017-12-17 23:28:07 +0400

    remove activation route from swagger as it causes problems with redirect

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -6,8 +6,19 @@ const validateToken = require('../../middlewares/validateToken');
 const AuthController = require('../../controllers/AuthController');
 const sendResponse = require('../../helpers/response');
 const codes = require('../../helpers/codes');
+const config = require('../../../config/default');
 
 const router = express.Router();
+
+router.get('/register/activate', (req, res) => {
+  AuthController.userActivation(req)
+    .then((data) => {
+      res.redirect(config.activation.redirectUrl);
+    })
+    .catch((err) => {
+      res.redirect(`${config.activation.redirectUrl}?err${err.message || 'Something went wrong'}`);
+    });
+});
 
 router.get('/login/twitter/callback', passport.authenticate('twitter'), (req, res) => {
   AuthController.createSession(req)
