c23a01501d67eb4c3f0b03502de4ef0ec3c3cce2
Edgar Markosov <edgar.markosov@gmail.com>
2017-12-20 02:43:05 +0400

    fixed tests, refactor twitter and github strategies to make possible for the test

--- passport/index.js
+++ passport/index.js
@@ -8,26 +8,128 @@ const GitHubStrategy = require('passport-github').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
 
+module.exports.localStrategyCallback = (email, password, done) => {
+  config.db.select().from('Users')
+    .where({
+      email,
+    })
+    .one()
+    .then((user) => {
+      if (!user) {
+        return done(codes.BadRequest('Incorrect email.'), false);
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(codes.BadRequest('Incorrect password.'), false);
+      }
+
+      return done(null, user);
+    })
+    .catch((err) => done(err));
+};
+
+module.exports.githubStrategyCallback = (accessToken, refreshToken, profile, cb) => {
+  // console.log('Github profile: ', profile);
+  let email;
+  let foundUser;
+  let query = 'SELECT @rid, email, githubId, twitterId FROM Users WHERE githubId = :githubId';
+  const params = {
+    githubId: profile.id,
+    regProvider: 'github',
+  };
+  if (profile.emails && profile.emails.length) {
+    profile.emails.forEach((item) => {
+      if (item.primary) {
+        email = item.value;
+      }
+    });
+  }
+  if (email) {
+    params.email = email;
+    query += ' OR email = :email';
+  }
+  config.db.query(
+    query,
+    {
+      params,
+    },
+  )
+    .then((user) => {
+      if (user && user.length) {
+        foundUser = user[0];
+        return user[0];
+      }
+      return config.db.class.get('Users');
+    })
+    .then((Users) => {
+      if (foundUser) {
+        if (!foundUser.githubId) {
+          foundUser.githubId = profile.id;
+          return config.db.update(foundUser.rid).set({ githubId: profile.id }).one();
+        }
+        return foundUser;
+      }
+
+      return Users.create(params);
+    })
+    .then((user) => {
+      // console.log(user);
+      foundUser = foundUser || user;
+      cb(null, foundUser);
+    })
+    .catch((err) => cb(err));
+};
+
+module.exports.twitterStrategyCallback = (token, tokenSecret, profile, cb) => {
+  // console.log('Twitter profile: ', profile);
+  let email;
+  let foundUser;
+  let query = 'SELECT @rid, email, githubId, twitterId FROM Users WHERE twitterId = :twitterId';
+  const params = {
+    twitterId: profile.id,
+    regProvider: 'twitter',
+  };
+  if (profile.emails && profile.emails.length) {
+    email = profile.emails[0].value;
+  }
+  if (email) {
+    params.email = email;
+    query += ' OR email = :email';
+  }
+  config.db.query(
+    query,
+    {
+      params,
+    },
+  )
+    .then((user) => {
+      if (user && user.length) {
+        foundUser = user[0];
+        return user[0];
+      }
+      return config.db.class.get('Users');
+    })
+    .then((Users) => {
+      if (foundUser) {
+        if (!foundUser.twitterId) {
+          foundUser.twitterId = profile.id;
+          return config.db.update(foundUser.rid).set({ twitterId: profile.id }).one();
+        }
+        return foundUser;
+      }
+
+      return Users.create(params);
+    })
+    .then((user) => {
+      // console.log(user);
+      foundUser = foundUser || user;
+      cb(null, foundUser);
+    })
+    .catch((err) => cb(err));
+};
+
 module.exports.configureStrategies = () => {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, ((email, password, done) => {
-    config.db.select().from('Users')
-      .where({
-        email,
-      })
-      .one()
-      .then((user) => {
-        if (!user) {
-          return done(codes.BadRequest('Incorrect email.'), false);
-        }
-
-        if (!bcrypt.compareSync(password, user.password)) {
-          return done(codes.BadRequest('Incorrect password.'), false);
-        }
-
-        return done(null, user);
-      })
-      .catch((err) => done(err));
-  })));
+  passport.use(new LocalStrategy({ usernameField: 'email' }, module.exports.localStrategyCallback));
 
   passport.use(new GitHubStrategy(
     {
@@ -36,57 +138,7 @@ module.exports.configureStrategies = () => {
       callbackURL: config.passport.github.callbackUrl,
       scope: ['user:email'],
     },
-    (accessToken, refreshToken, profile, cb) => {
-      // console.log('Github profile: ', profile);
-      let email;
-      let foundUser;
-      let query = 'SELECT @rid, email, githubId, twitterId FROM Users WHERE githubId = :githubId';
-      const params = {
-        githubId: profile.id,
-        regProvider: 'github',
-      };
-      if (profile.emails && profile.emails.length) {
-        profile.emails.forEach((item) => {
-          if (item.primary) {
-            email = item.value;
-          }
-        });
-      }
-      if (email) {
-        params.email = email;
-        query += ' OR email = :email';
-      }
-      config.db.query(
-        query,
-        {
-          params,
-        },
-      )
-        .then((user) => {
-          if (user && user.length) {
-            foundUser = user[0];
-            return user[0];
-          }
-          return config.db.class.get('Users');
-        })
-        .then((Users) => {
-          if (foundUser) {
-            if (!foundUser.githubId) {
-              foundUser.githubId = profile.id;
-              return config.db.update(foundUser.rid).set({ githubId: profile.id }).one();
-            }
-            return foundUser;
-          }
-
-          return Users.create(params);
-        })
-        .then((user) => {
-          // console.log(user);
-          foundUser = foundUser || user;
-          cb(null, foundUser);
-        })
-        .catch((err) => cb(err));
-    },
+    module.exports.githubStrategyCallback,
   ));
 
   passport.use(new TwitterStrategy(
@@ -96,53 +148,7 @@ module.exports.configureStrategies = () => {
       callbackURL: config.passport.twitter.callbackUrl,
       includeEmail: true,
     },
-    (token, tokenSecret, profile, cb) => {
-      // console.log('Twitter profile: ', profile);
-      let email;
-      let foundUser;
-      let query = 'SELECT @rid, email, githubId, twitterId FROM Users WHERE twitterId = :twitterId';
-      const params = {
-        twitterId: profile.id,
-        regProvider: 'twitter',
-      };
-      if (profile.emails && profile.emails.length) {
-        email = profile.emails[0].value;
-      }
-      if (email) {
-        params.email = email;
-        query += ' OR email = :email';
-      }
-      config.db.query(
-        query,
-        {
-          params,
-        },
-      )
-        .then((user) => {
-          if (user && user.length) {
-            foundUser = user[0];
-            return user[0];
-          }
-          return config.db.class.get('Users');
-        })
-        .then((Users) => {
-          if (foundUser) {
-            if (!foundUser.twitterId) {
-              foundUser.twitterId = profile.id;
-              return config.db.update(foundUser.rid).set({ twitterId: profile.id }).one();
-            }
-            return foundUser;
-          }
-
-          return Users.create(params);
-        })
-        .then((user) => {
-          // console.log(user);
-          foundUser = foundUser || user;
-          cb(null, foundUser);
-        })
-        .catch((err) => cb(err));
-    },
+    module.exports.twitterStrategyCallback,
   ));
 
   passport.serializeUser((user, done) => {
--- test/api/auth.js
+++ test/api/auth.js
@@ -4,6 +4,7 @@ const app = require('../../server');
 const request = require('supertest');
 const test = require('ava').test;
 const helpers = require('../helpers');
+const passport = require('../../passport');
 
 const SUCCESS = 200;
 const BAD_REQUEST = 400;
@@ -11,46 +12,8 @@ const REGISTER_ROUTE = '/auth/register';
 const LOGIN_ROUTE = '/auth/login';
 const REFRESH_TOKEN_ROUTE = '/auth/token/refresh';
 
-let refreshToken;
-
 test.before('Create DB', () => helpers.createDB());
-test.after('Truncate DB', () => { helpers.truncateDB(); });
-
-test.serial('register: Validation error, email pattern', async (t) => {
-  t.plan(2);
-
-  const res = await request(app)
-    .post(REGISTER_ROUTE)
-    .send({ email: 'testemail123456', password: '123456789test' });
-
-  t.is(res.status, BAD_REQUEST);
-  t.regex(res.body.message, /^Validation error/);
-});
-
-test.serial('register: Swagger SCHEMA_VALIDATION_FAILED, email required', async (t) => {
-  t.plan(2);
-
-  const res = await request(app)
-    .post(REGISTER_ROUTE)
-    .send({ password: '123456789test' });
-
-  t.is(res.status, BAD_REQUEST);
-  t.is(res.body.code, 'SCHEMA_VALIDATION_FAILED');
-});
-
-test.serial('register: Swagger SCHEMA_VALIDATION_FAILED, password required', async (t) => {
-  t.plan(2);
-
-  const res = await request(app)
-    .post(REGISTER_ROUTE)
-    .send({ email: 'test@gmail.com' });
-
-  t.is(res.status, BAD_REQUEST);
-  t.is(res.body.code, 'SCHEMA_VALIDATION_FAILED');
-});
-
-
-test.serial('register: Success', async (t) => {
+test.beforeEach(async (t) => {
   t.plan(5);
 
   const res = await request(app)
@@ -63,6 +26,50 @@ test.serial('register: Success', async (t) => {
   t.is(res.body.data.regProvider, 'local');
   t.truthy(res.body.data.userId);
 });
+test.afterEach.always(() => {
+  return helpers.truncateDB();
+});
+
+test('register: Validation error, email pattern', async (t) => {
+  t.plan(2);
+
+  const res = await request(app)
+    .post(REGISTER_ROUTE)
+    .send({ email: 'testemail123456', password: '123456789test' });
+
+  t.is(res.status, BAD_REQUEST);
+  t.regex(res.body.message, /^Validation error/);
+});
+
+test('register: Swagger SCHEMA_VALIDATION_FAILED, email required', async (t) => {
+  t.plan(6);
+
+  const res = await request(app)
+    .post(REGISTER_ROUTE)
+    .send({ password: '123456789test' });
+
+  t.is(res.status, BAD_REQUEST);
+  t.is(res.body.code, 'SCHEMA_VALIDATION_FAILED');
+  t.truthy(res.body.results);
+  t.truthy(res.body.results.errors);
+  t.truthy(res.body.results.errors[0].message);
+  t.is(res.body.results.errors[0].message, 'Missing required property: email');
+});
+
+test('register: Swagger SCHEMA_VALIDATION_FAILED, password required', async (t) => {
+  t.plan(6);
+
+  const res = await request(app)
+    .post(REGISTER_ROUTE)
+    .send({ email: 'test@gmail.com' });
+
+  t.is(res.status, BAD_REQUEST);
+  t.is(res.body.code, 'SCHEMA_VALIDATION_FAILED');
+  t.truthy(res.body.results);
+  t.truthy(res.body.results.errors);
+  t.truthy(res.body.results.errors[0].message);
+  t.is(res.body.results.errors[0].message, 'Missing required property: password');
+});
 
 test.serial('register: Email is taken', async (t) => {
   t.plan(1);
@@ -70,21 +77,33 @@ test.serial('register: Email is taken', async (t) => {
   const res = await request(app)
     .post(REGISTER_ROUTE)
     .send({ email: 'testemail123456@mailinator.com', password: '123456789test1' });
+
   t.is(res.status, BAD_REQUEST);
 });
 
 test.serial('login: Success', async (t) => {
-  t.plan(4);
+  t.plan(8);
 
   const res = await request(app)
     .post(LOGIN_ROUTE)
     .send({ email: 'testemail123456@mailinator.com', password: '123456789test' });
 
-  refreshToken = res.body.data.refreshToken;
   t.is(res.status, SUCCESS);
   t.truthy(res.body.data);
   t.truthy(res.body.data.accessToken);
   t.truthy(res.body.data.refreshToken);
+
+  t.log('refresh:token: Success');
+
+  const res1 = await request(app)
+    .post(REFRESH_TOKEN_ROUTE)
+    .set('x-refresh-token', res.body.data.refreshToken)
+    .send({});
+
+  t.is(res1.status, SUCCESS);
+  t.truthy(res1.body.data);
+  t.truthy(res1.body.data.accessToken);
+  t.truthy(res1.body.data.refreshToken);
 });
 
 test.serial('login: Wrong email', async (t) => {
@@ -109,16 +128,26 @@ test.serial('login: Wrong password', async (t) => {
   t.is(res.body.message, 'Incorrect password.');
 });
 
-test.serial('refresh:token: Success', async (t) => {
-  t.plan(4);
+test.cb('passport:github', (t) => {
+  t.plan(5);
+  passport.githubStrategyCallback(null, null, { id: '123456', emails: [{ value: 'testemail123456@mailinator.com', primary: true }] }, (err, user) => {
+    t.is(err, null);
+    t.truthy(user);
+    t.is(user.githubId, '123456');
+    t.is(user.email, 'testemail123456@mailinator.com');
+    t.pass();
+    t.end();
+  });
+});
 
-  const res = await request(app)
-    .post(REFRESH_TOKEN_ROUTE)
-    .set('x-refresh-token', refreshToken)
-    .send({});
-
-  t.is(res.status, SUCCESS);
-  t.truthy(res.body.data);
-  t.truthy(res.body.data.accessToken);
-  t.truthy(res.body.data.refreshToken);
+test.cb('passport:twitter', (t) => {
+  t.plan(5);
+  passport.twitterStrategyCallback(null, null, { id: '654321', emails: [{ value: 'testemail123456@mailinator.com' }] }, (err, user) => {
+    t.is(err, null);
+    t.truthy(user);
+    t.is(user.twitterId, '654321');
+    t.is(user.email, 'testemail123456@mailinator.com');
+    t.pass();
+    t.end();
+  });
 });
