71f396ab8f4a6a7f2deb822585e8527368eaafbb
Georgiana Bere <bere.georgi@gmail.com>
2017-12-25 18:14:15 +0200

    Respect code structure in account tests

--- test/api/controllers/account.js
+++ test/api/controllers/account.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const _ = require('lodash');
-const app = require('../../server');
-const config = require('../../config/default');
-const AuthController = require('../../api/controllers/AuthController');
 const request = require('supertest');
 const test = require('ava').test;
-const helpers = require('../helpers');
-const passport = require('../../passport');
+const helpers = require('../../helpers');
+const app = require('../../../server');
+const config = require('../../../config/default');
+const AuthController = require('../../../api/controllers/AuthController');
 
 const SUCCESS = 200;
 const CREATED = 201;
@@ -92,6 +91,28 @@ test.serial('register: Email is taken', async (t) => {
   t.regex(res.body.errors[0].message, /taken/i);
 });
 
+test.serial('getAccount returns the account associated with tokens', async (t) => {
+  const userAttrs = {
+    id: 'alabala',
+    active: true,
+    email: 'testemail123456@mailinator.com',
+    password: await AuthController.createPasswordHash('123456789test'),
+  };
+  const completeUser = await AuthController.createTokenPair({ id: userAttrs.id })
+    .then((tokens) => Object.assign(userAttrs, {
+      accessTokens: [tokens.accessToken],
+      refreshTokens: [tokens.refreshToken],
+    }));
+  await config.db.class.get('User')
+    .then((User) => User.create(completeUser));
+  const res = await request(app)
+    .get(REGISTER_ROUTE)
+    .set('x-access-token', completeUser.accessTokens[0])
+    .set('x-refresh-token', completeUser.refreshTokens[0]);
+  t.is(res.status, SUCCESS);
+  t.is(res.body.id, userAttrs.id);
+});
+
 test.serial('login: Success', async (t) => {
   const userCreds = {
     email: 'testemail123456@mailinator.com',
@@ -144,24 +165,22 @@ test.serial('login: Wrong password', async (t) => {
   t.regex(res.body.errors[0].message, /password/i);
 });
 
-test.serial('getAccount returns the account associated with tokens', async (t) => {
-  const userAttrs = {
-    id: 'alabala',
-    active: true,
+test.serial('login: Wrong password', async (t) => {
+  t.plan(3);
+  const userCreds = {
     email: 'testemail123456@mailinator.com',
+    active: true,
     password: await AuthController.createPasswordHash('123456789test'),
   };
-  const completeUser = await AuthController.createTokenPair({ id: userAttrs.id })
-    .then((tokens) => Object.assign(userAttrs, {
-      accessTokens: [tokens.accessToken],
-      refreshTokens: [tokens.refreshToken],
-    }));
   await config.db.class.get('User')
-    .then((User) => User.create(completeUser));
+    .then((User) => User.create(userCreds));
+
   const res = await request(app)
-    .get(REGISTER_ROUTE)
-    .set('x-access-token', completeUser.accessTokens[0])
-    .set('x-refresh-token', completeUser.refreshTokens[0]);
-  t.is(res.status, SUCCESS);
-  t.is(res.body.id, userAttrs.id);
+    .post(LOGIN_ROUTE)
+    .send({ email: userCreds.email, password: 'wrong_password' });
+
+  t.is(res.status, BAD_REQUEST);
+  t.regex(res.body.errors[0].message, /incorrect/i);
+  t.regex(res.body.errors[0].message, /password/i);
 });
+
