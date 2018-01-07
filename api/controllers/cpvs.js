99b4a23a7ad4dedc2247b777806260cde074d6a8
Georgiana Bere <bere.georgi@gmail.com>
2018-01-07 23:46:30 +0200

    Show only CPVs present in tenders

--- api/controllers/cpvs.js
+++ api/controllers/cpvs.js
@@ -5,44 +5,37 @@ const codes = require('../helpers/codes');
 const config = require('../../config/default');
 const formatError = require('../helpers/errorFormatter');
 
-function listCpvs(req, res) {
+function getTenderCpvs(req, res) {
   const swaggerParams = _.pickBy(
     _.mapValues(req.swagger.params, 'value'),
     (val) => !(_.isUndefined(val)),
   );
-  let cpvs;
-  if (_.isEmpty(swaggerParams)) {
-    cpvs = config.db.select().from('CPV').all();
-  } else {
-    const queryCriteria = [];
-    const queryParams = {};
-    if (swaggerParams.countries) {
-      queryCriteria.push('{ as: bids,  where: (xCountry in :countries) }');
-      queryParams.countries = swaggerParams.countries;
-    }
-    if (swaggerParams.years) {
-      queryCriteria.push('{ as: bids,  where: (xYear in :years) }');
-      queryParams.years = swaggerParams.years;
-    }
-    if (swaggerParams.buyers) {
-      queryCriteria.push(`{ as: bids }.in('Awards')
-        { class: Buyer, where: (id in :buyers) }`);
-      queryParams.buyers = swaggerParams.buyers;
-    }
-    if (swaggerParams.bidders) {
-      queryCriteria.push(`{ as: bids }.in('Participates')
-        { class: Bidder, where: (id in :bidders) }`);
-      queryParams.bidders = swaggerParams.bidders;
-    }
-    const query = `SELECT expand(cpvs) FROM (
-      MATCH { class: Bid, as: bids },
-        ${_.join(queryCriteria, ',')},
-        { as: bids }.out('AppliedTo').in('Comprises').out('HasCPV'){ as: cpvs }
-      RETURN cpvs
-    )`;
-    cpvs = config.db.query(query, { params: queryParams });
+  const queryCriteria = ["{ as: bids }.out('AppliedTo').in('Comprises').out('HasCPV'){ as: cpvs }"];
+  const queryParams = {};
+  if (swaggerParams.countries) {
+    queryCriteria.push('{ as: bids,  where: (xCountry in :countries) }');
+    queryParams.countries = swaggerParams.countries;
   }
-  return cpvs
+  if (swaggerParams.years) {
+    queryCriteria.push('{ as: bids,  where: (xYear in :years) }');
+    queryParams.years = swaggerParams.years;
+  }
+  if (swaggerParams.buyers) {
+    queryCriteria.push(`{ as: bids }.in('Awards')
+      { class: Buyer, where: (id in :buyers) }`);
+    queryParams.buyers = swaggerParams.buyers;
+  }
+  if (swaggerParams.bidders) {
+    queryCriteria.push(`{ as: bids }.in('Participates')
+      { class: Bidder, where: (id in :bidders) }`);
+    queryParams.bidders = swaggerParams.bidders;
+  }
+  const query = `SELECT expand(cpvs) FROM (
+    MATCH { class: Bid, as: bids },
+      ${_.join(queryCriteria, ',')}
+    RETURN cpvs
+  )`;
+  return config.db.query(query, { params: queryParams })
     .then((results) => res.status(codes.SUCCESS).json({
       cpvs: _.map(results, (cpv) => formatCpv(cpv)),
     }))
@@ -54,6 +47,6 @@ function formatCpv(cpvNode) {
 }
 
 module.exports = {
-  listCpvs,
+  getTenderCpvs,
   formatCpv,
 };
