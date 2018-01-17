6cc6e0a3bfc692e21e34690dbe67437bf7bdf1f0
Georgiana Bere <bere.georgi@gmail.com>
2018-01-17 11:04:31 +0200

    Add bidders and tests for actor autocomplete

--- api/controllers/actors.js
+++ api/controllers/actors.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const Promise = require('bluebird');
 const codes = require('../helpers/codes');
 const config = require('../../config/default');
 const formatError = require('../helpers/errorFormatter');
@@ -10,44 +11,54 @@ function getTenderActors(req, res) {
     _.mapValues(req.swagger.params, 'value'),
     (val) => !(_.isUndefined(val)),
   );
-  // Get only buyers involved in bids
-  const queryCriteria = [
-    '{ class: Buyer, as: buyers }',
-    "{ as: buyers }.out('Awards'){ class: Bid, as: bids}",
-  ];
-  const queryParams = {};
-  if (swaggerParams.name) {
-    queryCriteria.push('{ as: buyers, where: (name LUCENE :nameQuery) }');
-    queryParams.nameQuery = swaggerParams.name;
-    if (_.isEmpty(swaggerParams.name.match(/~\d?$/))) {
-      queryParams.nameQuery = `${swaggerParams.name}*`;
-    }
-  }
-  if (swaggerParams.countries) {
-    queryCriteria.push('{ as: bids,  where: (xCountry in :countries) }');
-    queryParams.countries = swaggerParams.countries;
-  }
-  if (swaggerParams.years) {
-    queryCriteria.push('{ as: bids,  where: (xYear in :years) }');
-    queryParams.years = swaggerParams.years;
-  }
-  if (swaggerParams.cpvs) {
-    queryCriteria.push(`{ as: bids }.out('AppliedTo').in('Comprises').out('HasCPV')
-      { class: CPV, where: (code in :cpvs)}`);
-    queryParams.cpvs = swaggerParams.cpvs;
-  }
-  const query = `SELECT expand(buyers) FROM (
-    MATCH ${_.join(queryCriteria, ',')}
-    RETURN buyers
-  )`;
-  return config.db.query(query, { params: queryParams })
+  return Promise.join(
+    getActors(swaggerParams, 'Buyer', 'Awards'),
+    getActors(swaggerParams, 'Bidder', 'Participates'),
+    (buyers, bidders) => _
+      .chain(buyers)
+      .concat(bidders)
+      .sortBy('$score')
+      .value(),
+  )
     .then((results) => res.status(codes.SUCCESS).json({
-      actors: _.map(results, (buyer) => formatBuyer(buyer)),
+      actors: _.map(results, (buyer) => formatActor(buyer)),
     }))
     .catch((err) => formatError(err, req, res));
 }
 
-function formatBuyer(buyerNode) {
+function getActors(swaggerParams, actorClass, edgeToBidClass) {
+  // Get only buyers involved in bids
+  const queryParams = {};
+  const queryCriteria = [];
+  let from = actorClass;
+  if (swaggerParams.name) {
+    queryParams.nameQuery = swaggerParams.name;
+    // If the user didn't set fuzziness make this a prefix query
+    if (_.isEmpty(swaggerParams.name.match(/~\d?$/))) {
+      queryParams.nameQuery = `${swaggerParams.name}*`;
+    }
+    from = `(SELECT *, $score FROM ${actorClass} WHERE name LUCENE :nameQuery)`;
+  }
+  if (swaggerParams.years) {
+    queryCriteria.push(`out('${edgeToBidClass}').xYear in :years`);
+    queryParams.years = swaggerParams.years;
+  }
+  if (swaggerParams.countries) {
+    queryCriteria.push(`out('${edgeToBidClass}').xCountry in :countries`);
+    queryParams.countries = swaggerParams.countries;
+  }
+  if (swaggerParams.cpvs) {
+    queryCriteria.push(`out('${edgeToBidClass}').out('AppliedTo').in('Comprises').out('HasCPV').code IN :cpvs`);
+    queryParams.cpvs = swaggerParams.cpvs;
+  }
+  let query = `SELECT *, $score FROM ${from}`;
+  if (_.isEmpty(queryCriteria) === false) {
+    query = query.concat(` WHERE ${_.join(queryCriteria, 'AND')}`);
+  }
+  return config.db.query(query, { params: queryParams });
+}
+
+function formatActor(buyerNode) {
   const buyerResponse = _.pick(buyerNode, ['name', 'id']);
   buyerResponse.type = _.toLower(buyerNode['@class']);
   return buyerResponse;
@@ -55,5 +66,6 @@ function formatBuyer(buyerNode) {
 
 module.exports = {
   getTenderActors,
-  formatBuyer,
+  formatActor,
+  getActors,
 };
