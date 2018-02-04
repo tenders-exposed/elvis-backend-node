83b23614b066b15d4d32432d1d0b018c70e48fdf
Georgiana Bere <bere.georgi@gmail.com>
2018-02-04 13:54:21 +0200

    Update network schema

---
+++ migrations/m20180203_152833_drop_id_from_network_edge.js
@@ -0,0 +1,32 @@
+'use strict';
+
+exports.name = 'create network base edge';
+
+exports.up = (db) => (
+  db.class.create('NetworkEdge', 'E')
+    .then((NetworkEdge) => {
+      NetworkEdge.property.create([
+        {
+          name: 'id',
+          type: 'String',
+          mandatory: true,
+        },
+        {
+          name: 'visible',
+          type: 'Boolean',
+          mandatory: true,
+          default: true,
+        },
+      ]);
+    })
+    .then(() => {
+      db.index.create({
+        name: 'NetworkEdge.id',
+        type: 'unique',
+      });
+    })
+);
+
+exports.down = (db) => (
+  db.class.drop('NetworkEdge')
+);
