90cacd9def13331ece2377a6c0937702aa863ba3
Georgiana Bere <bere.georgi@gmail.com>
2018-02-11 11:52:40 +0200

    List all networks or get a network by id

--- api/controllers/networks.js
+++ api/controllers/networks.js
@@ -13,7 +13,7 @@ function createNetwork(req, res) {
   const networkParams = req.swagger.params.body.value;
   return validateToken(req, res, () =>
     writers.createNetwork(networkParams, req.user))
-    .then((network) => formatNetwork(network))
+    .then((network) => formatNetworkWithRelated(network))
     .then((network) => res.status(codes.CREATED).json(network))
     .catch((err) => formatError(err, req, res));
 }
@@ -41,59 +41,82 @@ function deleteNetwork(req, res) {
   }).catch((err) => formatError(err, req, res));
 }
 
+function getNetwork(req, res) {
+  const networkID = req.swagger.params.networkID.value;
+  return config.db.select()
+    .from('Network')
+    .where({ id: networkID })
+    .one()
+    .then((network) => formatNetworkWithRelated(network))
+    .then((network) => res.status(codes.SUCCESS).json(network))
+    .catch((err) => formatError(err, req, res));
+}
+
+function getNetworks(req, res) {
+  return validateToken(req, res, () => {
+    if (_.isUndefined(req.user) === false) {
+      return config.db.select()
+        .from('Network')
+        .where({ "in('Owns').id": req.user.id })
+        .all()
+        .then((networks) =>
+          res.status(codes.SUCCESS).json(_.map(networks, (network) => formatNetwork(network))));
+    }
+    throw codes.Unauthorized('This operation requires authorization.');
+  }).catch((err) => formatError(err, req, res));
+}
+
 function formatNetwork(network) {
+  const prettyNetwork = _.pick(network, ['id', 'name', 'synopsis']);
+  prettyNetwork.settings = _.pick(network.settings, ['nodeSize', 'edgeSize']);
+  prettyNetwork.query = _.pick(network.query, ['countries', 'years', 'cpvs', 'bidders', 'buyers']);
+  return prettyNetwork;
+}
+
+function formatNetworkWithRelated(network) {
+  const prettyNetwork = formatNetwork(network);
   const networkID = network.id;
-  const networkQuery = `SELECT *
-    FROM Network
-    WHERE id=:networkID;`;
-  return config.db.query(networkQuery, { params: { networkID: network.id } })
-    .then((results) => {
-      const rawNetwork = results[0];
-      const prettyNetwork = _.pick(rawNetwork, ['id', 'name', 'synopsis']);
-      prettyNetwork.settings = _.pick(rawNetwork.settings, ['nodeSize', 'edgeSize']);
-      prettyNetwork.query = _.pick(rawNetwork.query, ['countries', 'years', 'cpvs', 'bidders', 'buyers']);
+  const nodesQuery = `SELECT *
+    FROM NetworkActor
+    WHERE out('PartOf').id=:networkID
+    AND visible=true;`;
+  const edgesQuery = (className) => `SELECT *,
+    in.id as \`from\`,
+    out.id as to,
+    @class.toLowerCase() as type
+    FROM ${className}
+    WHERE visible=true
+    AND out.out('PartOf').id=:networkID;`;
+  const userQuery = `SELECT *
+    FROM User
+    WHERE out('Owns').id in [:networkID];`;
+  return Promise.join(
+    config.db.query(nodesQuery, { params: { networkID } }),
+    config.db.query(edgesQuery('Contracts'), { params: { networkID } }),
+    config.db.query(edgesQuery('Partners'), { params: { networkID } }),
+    config.db.query(userQuery, { params: { networkID } }),
+    (nodes, contractsEdges, partnersEdges, users) => {
+      prettyNetwork.nodes = nodes.map((node) =>
+        _.pick(node, ['label', 'id', 'type', 'medianCompetition', 'value', 'country']));
+      prettyNetwork.edges = _.concat(contractsEdges, partnersEdges).map((edge) =>
+        _.pick(edge, ['from', 'to', 'type', 'value']));
+      prettyNetwork.count = {
+        nodes: prettyNetwork.nodes.length,
+        edges: prettyNetwork.edges.length,
+      };
+      if (_.isUndefined(users[0]) === false) {
+        prettyNetwork.user = _.pick(users[0], ['id', 'email']);
+      }
       return prettyNetwork;
-    })
-    .then((prettyNetwork) => {
-      const nodesQuery = `SELECT *
-        FROM NetworkActor
-        WHERE out('PartOf').id=:networkID
-        AND visible=true;`;
-      const edgesQuery = (className) => `SELECT *,
-        in.id as \`from\`,
-        out.id as to,
-        @class.toLowerCase() as type
-        FROM ${className}
-        WHERE visible=true
-        AND out.out('PartOf').id=:networkID;`;
-      const userQuery = `SELECT *
-        FROM User
-        WHERE out('Owns').id in [:networkID];`;
-      return Promise.join(
-        config.db.query(nodesQuery, { params: { networkID } }),
-        config.db.query(edgesQuery('Contracts'), { params: { networkID } }),
-        config.db.query(edgesQuery('Partners'), { params: { networkID } }),
-        config.db.query(userQuery, { params: { networkID } }),
-        (nodes, contractsEdges, partnersEdges, users) => {
-          prettyNetwork.nodes = nodes.map((node) =>
-            _.pick(node, ['label', 'id', 'type', 'medianCompetition', 'value', 'country']));
-          prettyNetwork.edges = _.concat(contractsEdges, partnersEdges).map((edge) =>
-            _.pick(edge, ['from', 'to', 'type', 'value']));
-          prettyNetwork.count = {
-            nodes: prettyNetwork.nodes.length,
-            edges: prettyNetwork.edges.length,
-          };
-          if (_.isUndefined(users[0]) === false) {
-            prettyNetwork.user = _.pick(users[0], ['id', 'email']);
-          }
-          return prettyNetwork;
-        },
-      );
-    });
+    },
+  );
 }
 
 module.exports = {
   createNetwork,
   deleteNetwork,
+  getNetwork,
+  getNetworks,
   formatNetwork,
+  formatNetworkWithRelated,
 };
