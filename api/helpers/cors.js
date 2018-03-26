555116fcd8f6756a859e860a8c47d63bf99399b8
Victor Nițu <victor@thesponge.eu>
2018-03-26 12:33:14 +0300

    Add CORS headers on non Swagger requests

---
+++ api/helpers/cors.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const url = require('url');
+
+module.exports = (req, res, next) => {
+  const originURL = url.parse(req.headers.referer);
+  const originReferer = `${originURL.protocol}//${originURL.hostname}`;
+  const originHeader = req.headers.origin;
+
+  const origin = originHeader || originReferer || '*';
+
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-type,Accept,X-Refresh-Token,Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return next();
+};
