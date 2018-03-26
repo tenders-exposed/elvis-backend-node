8b0db5878ec3f92060cca8308e035f60d75e8c00
Victor Nițu <victor@thesponge.eu>
2018-03-26 16:20:48 +0300

    Early return for OPTIONS request

--- api/helpers/cors.js
+++ api/helpers/cors.js
@@ -3,6 +3,10 @@
 const url = require('url');
 
 module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const originURL = url.parse(req.headers.referer);
   const originReferer = `${originURL.protocol}//${originURL.hostname}`;
   const originHeader = req.headers.origin;
@@ -13,10 +17,6 @@ module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-type,Accept,X-Refresh-Token,Authorization');
   res.header('Access-Control-Allow-Credentials', true);
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
 
   return next();
 };
